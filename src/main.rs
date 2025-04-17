63cc91c7d8ce8c067e82cde449d81abea1ed6a5e
Bob Osola <bobosola@gmail.com>
2025-04-17 22:39:22 +0100

    1st use of libs

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use std::fs::File;
-use std::io::BufReader;
-use mp4::creation_time;
-use std::time::{ UNIX_EPOCH, Duration };
+use std::time::{SystemTime, UNIX_EPOCH};
+use std::path::Path;
 use std::fs::FileTimes;
 use walkdir::{DirEntry, WalkDir};
-use anyhow::Result;
+use anyhow::{ anyhow, Context, Result };
+use file_format::FileFormat;
+use nom_exif::*;
+use nom_exif::ExifTag::*;
+use chrono::{ DateTime, Tz };
 
 fn main() -> Result<()> {
 
@@ -12,46 +15,86 @@ fn main() -> Result<()> {
 
     for entry in WalkDir::new(dir_path).into_iter().filter_map(|e| e.ok()) {
 
-        if !is_mp4(&entry) {
-            continue;
-        }
-
-        let path = entry.path();
-
-        // Get MP4 metadata
-        let file = File::open(path)?;
-        let filesize = file.metadata()?.len();
-        let reader = BufReader::new(file);
-        let mp4 = mp4::Mp4Reader::read_header(reader, filesize)?;
-
-        // Get MP4 creation datetime (a Unix timestamp) from metadata
-        // and convert to SystemTime
-        let ts = creation_time(mp4.moov.mvhd.creation_time);
-        let created_date = UNIX_EPOCH + Duration::from_secs(ts);
-
-        // Prepare the date to be used on a file
-        let filetimes = FileTimes::new().set_modified(created_date);
-
-        // Change the 'file modified' date to the metadata creation date
-        let file_to_amend = File::options().write(true).open(path)?;
-        file_to_amend.set_times(filetimes)?;
-        println!("Converted: {}", path.display());
-    }
-
-    fn is_mp4(entry: &DirEntry) -> bool {
-
-        let filename = match entry.file_name().to_str() {
-            Some(name) => name,
-            None => return false
+        let file_path = entry.path();
+        let file_format = FileFormat::from_file(file_path).context("Could not determine file format")?;
+        match parse_file(file_format, file_path) {
+            Ok(systime) => amend_file_date(systime, file_path),
+            _ => ()
         };
 
-        if entry.file_type().is_file() && 
-           filename.to_lowercase().ends_with("mp4") && 
-           !filename.starts_with(".") {
-            return true
-        }
-        false
     }
-
     Ok(())
 }
+
+    fn parse_file(file_format: FileFormat, file_path: &Path) -> Result<SystemTime> {
+
+        match file_format {
+            // .jpg image file
+            FileFormat::JointPhotographicExpertsGroup => {
+                let f = File::open(file_path)?;
+                match parse_jpeg_exif(f)? {
+                    Some(exif) => {
+                        return get_creation_date_from_exif(exif)
+                    },
+                    _ => ()
+                }            
+            },
+            // .heic or .heif image file
+            FileFormat::HighEfficiencyImageCoding | FileFormat::HighEfficiencyImageFileFormat=> {
+                let f = File::open(file_path)?;
+                match parse_heif_exif(f)? {
+                    Some(exif) => {
+                        return get_creation_date_from_exif(exif)
+                    },
+                    _ => ()
+                }            
+            },            
+            _ => ()
+        }
+        Err(anyhow!("File format {:?}", file_path))
+    }
+
+    fn get_exif (fmt: FileFormat, file_path: &Path) -> Result<Option<Exif>> {
+
+        let f = File::open(file_path).with_context(|| format!("Could not open file {:?}", file_path))?;
+
+        let exif = match fmt {
+            FileFormat::JointPhotographicExpertsGroup => {
+                match parse_jpeg_exif(f) {
+                    Ok(ex) => ex,
+                    _ => None
+                } 
+            },
+            FileFormat::HighEfficiencyImageCoding | FileFormat::HighEfficiencyImageFileFormat => {
+                match parse_heif_exif(f) {
+                    Ok(ex) => ex,
+                    _ => None
+                } 
+            },
+           _ => None
+        };
+        Ok(exif)
+    }
+
+    fn get_creation_date_from_exif(exif: Exif) -> Result<SystemTime> {
+
+        match exif.get_value(&CreateDate)? {
+            Some(val) => {
+                let dt = DateTime::parse_from_rfc3339(&val.to_string()).context("Non-standard EXIF date format")?;
+                Ok(SystemTime::from(dt))      
+            },
+            None => Err(anyhow!("EXIF data does not contain a created date"))
+        }
+    }
+
+    fn amend_file_date(dt: SystemTime, file_path: &Path) -> anyhow::Result<()> {
+
+        // Prepare the date to be used on the file
+        let filetimes = FileTimes::new().set_modified(dt);
+
+        // Change the 'file modified' date to the found metadata date
+        let file_to_amend = File::options().write(true).open(file_path)?;
+        file_to_amend.set_times(filetimes)?;
+        println!("Converted: {}", file_path.display());
+        Ok(())
+    }
