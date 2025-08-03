70f89ea8616dd6e19bfa455e55a3a07ba23b002e
Bob Osola <bobosola@gmail.com>
2025-08-03 22:28:31 +0100

    update batch files

--- win_app/bat/release_build.bat
+++ win_app/bat/release_build.bat
@@ -4,5 +4,5 @@ cargo build --target=x86_64-pc-windows-msvc --release
 :: Copy the DLL to the windows application
 SET PROJECT_DIR=C:\Users\Bob\rust\media-file-date-fixer
 SET DLL="%PROJECT_DIR%\target\x86_64-pc-windows-msvc\release\mfdf_ffi.dll"
-SET DESTINATION="%PROJECT_DIR%\win_app\mfdf\mfdf_ffi.dll"
+SET DESTINATION="%PROJECT_DIR%\win_app\mfdf"
 copy /y %DLL% %DESTINATION%
