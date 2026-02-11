3f0fae506194aa4a9823bd884b53c67ce13d4574
Bob Osola <bobosola@gmail.com>
2026-02-11 00:17:06 +0000

    fix script paths

--- mac_app/scripts/createDmg.sh
+++ mac_app/scripts/createDmg.sh
@@ -2,10 +2,10 @@
 # 
 # https://github.com/sindresorhus/create-dmg
 
-RELEASE_NAME="mfdf-2026-02-10-23-46-08"
-APP_NAME="mfdf"
+RELEASE_NAME="mfdf 2026-02-10 23-46-08"
+APP_NAME="mfdf.app"
 PROJECT_PATH="/Users/bobosola/rust/media-file-date-fixer/mac_app"
-APP_PATH="$PROJECT_PATH/Builds/Releases/$RELEASE_NAME/$APP_NAME.app"
-DEST_PATH="$PROJECT_PATH/Builds/Releases"
+APP_PATH="$PROJECT_PATH/Releases/$RELEASE_NAME/$APP_NAME"
+DEST_PATH="$PROJECT_PATH/Releases"
 
-create-dmg $APP_PATH $DEST_PATH
+create-dmg "$APP_PATH" "$DEST_PATH"
